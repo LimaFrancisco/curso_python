6c4a94f99be02320faa3251d1e190b629a8ca20a
FranciscoLima <terroristduck@DESKTOP-A5UI5FS.localdomain>
2025-11-06 10:52:16 -0300

    first commit: create aula1.py

---
+++ aula1.py
@@ -0,0 +1,16 @@
+"""
+DocString
+E escrever o que eu 
+quiser
+
+
+qwerqwerqwer
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
+
