9d8404589429f90056dce690a80fad819f1cc287
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-11-08 13:47:29 -0300

    Data conversion

---
+++ aula6.py
@@ -0,0 +1,12 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coersion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+print(1 + 1)  # Soma
+print("a" + "b")  # Concatenação de Strings
+print(int("1"), type(int("1")))
+
+print(type(float("1") + 1))
+print(bool(""))
+print(str(11) + "b")
