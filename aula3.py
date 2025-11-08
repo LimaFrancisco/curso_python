c0dc8cfb55f35bbdddce758e2c8195c51bbf78f7
FranciscoLima <terroristduck@DESKTOP-A5UI5FS.localdomain>
2025-11-08 12:50:58 -0300

    Strings

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print("Luiz Otávio")
+
+# Aspas duplas
+print("Luiz Otávio")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio")
+
+# Aspas duplas dentro de aspas simples
+print('Luiz "Otávio"')
