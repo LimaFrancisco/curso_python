018759a8c7266e6cecb5fbb6f399487f40788a7e
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-11-22 14:27:14 -0300

    debuging code

---
+++ aula18.py
@@ -0,0 +1,20 @@
+# if / elif        / else
+# se / se não se   / se não
+condicao1 = True
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print("Código para caondição 1 ")
+elif condicao2:
+    print("Código para caondição 2 ")
+elif condicao3:
+    print("Código para caondição 3 ")
+elif condicao4:
+    print("Código para caondição 4 ")
+else:
+    print("Nenhuma condição foi satisfeita")
+
+if 10 == 10:
+    print("Outro if")
