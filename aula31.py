b511598e3f51965ca486a9296b09010391e786bc
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-12-05 17:03:52 -0300

    id of the value in memory

---
+++ aula31.py
@@ -0,0 +1,23 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = "a"
+v2 = "a"
+print(id(v1))
+print(id(v2))
+
+""""
+O python reconhece que as duas variáveis tem o mesmo valor
+Então as duas apontam para o mesmo lugar da memória
+"""
+
+# condição = False
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#   print('Não faça algo')
